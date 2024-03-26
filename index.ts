118c67905b1ed9e5c76c2e352747c83b23e27f78
jhukegaNahiSaala <shaikhurwa61@gmail.com>
2024-03-25 17:39:46 -0700

    pushed my calculator to github

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+
+import inquirer from 'inquirer';
+
+const answer = await inquirer.prompt([
+{message: "Enter first number" , type: "number" , name: "firstNumber"},
+{message: "Enter second number" , type: "number" , name: "secondNumber"},
+{message: "Enter second number" , type: "list" , name: "options", choices: ["+", "-", "*", "/"]}
+])
+
+if (answer.options === "+") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.options === "-") {
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.options === "*") {
+    console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.options === "/") {
+  console.log(answer.firstNumber / answer.secondNumber);
+}
+
+// console.log(eval(answer.firstNumber + answer.options + answer.secondNumber));
